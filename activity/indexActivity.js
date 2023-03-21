c0831693f4729e291af0c3d3816f9d240262fda9
Matthew Lumugdang <lmh0010dlsud@gmail.com>
2023-03-21 09:08:58 +0800

    Added Main Activity 230321

---
+++ activity/indexActivity.js
@@ -0,0 +1,19 @@
+console.log("Hello, Welcome to De La Salle University!");
+let fName = 'Matthew';
+let lName = 'Lumugdang';
+let age = 22;
+let hobbies = ['Sleeping', 'Gaming', 'Sleeping again', 'Contemplating life choices', 'Watching youtube videos'];
+let homeAddress = {
+    houseNumber: 32,
+    street: 'Banana St.',
+    subdivision: 'Greensborough',
+    barangay: 'Bayan Luma 5',
+    city: 'Imus',
+    province: 'Cavite',
+    zipCode: 4103
+}
+
+console.log(`Hi! This is ${fName} ${lName}, who is currently ${age} years old who has hobbies which are:`);
+console.log(hobbies);
+console.log( "and lives in: ");
+console.log(homeAddress);
